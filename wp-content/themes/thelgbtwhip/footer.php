19b5629c775fb367e72d2e5084cb3ecb03325cb9
Matt Ward <matthew.ward@byng-systems.com>
2014-08-31 10:54:53 +0100

    Incorporated service-layer driven JS changes.

--- wp-content/themes/thelgbtwhip/footer.php
+++ wp-content/themes/thelgbtwhip/footer.php
@@ -80,47 +80,90 @@ if ($pagename == "constituency") {
                             {
                                 $('#cand' + (i + 1) + 'wikipedia').hide();
                             }
-
+                            
+                            var candidateId = i + 1;
+                            
+                            for (var j in arConstituency["candidates"][i]["voting_record"]) {
+                                var currentVote = arConstituency["candidates"][i]["voting_record"][j];
+                                
+                                console.log(currentVote);
+                        
+                                var vote = currentVote['vote'];
+                                var image = getImageName(vote);
+                                var title = getTooltip(vote);
+                                
+                                if (currentVote["bill"] == "Reduce age of consent for homosexual acts to 16")
+                                {
+                                    $("#cand" + candidateId + "vote1").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
+                                }
+                                else if (currentVote["bill"] == "Introduction of civil partnerships")
+                                {
+                                    $("#cand" + candidateId + "vote2").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
+                                }
+                                else if (currentVote["bill"] == "Equality Act (Sexual Orientation) Regulations")
+                                {
+                                    $("#cand" + candidateId + "vote3").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
+                                }
+                                else if (currentVote["bill"] == "IVF treatment for lesbian couples")
+                                {
+                                    $("#cand" + candidateId + "vote4").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
+                                }
+                                else if (currentVote["bill"] == "Marriage (Same-sex Couples) Bill")
+                                {
+                                    $("#cand" + candidateId + "vote5").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
+                                }
+                                else if (currentVote["bill"] == "Reduce age of consent for homosexual acts to 16")
+                                {
+                                    $("#cand" + candidateId + "vote6").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
+                                }
+                            }
+                            
+                            j = 0;
+                            
                             // Now go through the issues
                             for (var j = 0; j < arConstituency["candidates"][i]["issues"].length; j++)
                             {
                                 var issues = arConstituency["candidates"][i]["issues"][j];
                                 
+                                var vote = arConstituency["candidates"][i]["issues"][j]['vote'];
+                                var image = getImageName(vote);
+                                var title = getTooltip(vote);
+                                
                                 if (issues["issue_description"] == "Reduce age of consent for homosexual acts to 16")
                                 {
-                                    
+                                    $("#cand" + candidateId + "issue1").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Introduction of civil partnerships")
                                 {
-
+                                    $("#cand" + candidateId + "issue2").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Equality Act (Sexual Orientation) Regulations")
                                 {
-
+                                    $("#cand" + candidateId + "issue3").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "IVF treatment for lesbian couples")
                                 {
-
+                                    $("#cand" + candidateId + "issue4").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Marriage (Same-sex Couples) Bill")
                                 {
-
+                                    $("#cand" + candidateId + "issue5").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Reduce age of consent for homosexual acts to 16")
                                 {
-
+                                    $("#cand" + candidateId + "issue6").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Repeal of the 12 month deferral of men who have sex with men  (MSM) from donating blood.")
                                 {
-
+                                    $("#cand" + candidateId + "issue7").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Asylum in the UK for LGBT+ people fleeing persecution")
                                 {
-
+                                    $("#cand" + candidateId + "issue8").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                                 else if (issues["issue_description"] == "Recognition of trans* identities in all public services")
                                 {
-
+                                    $("#cand" + candidateId + "issue9").attr("src", "<?= get_template_directory_uri(); ?>/img/" + image + ".png").attr("title", title);
                                 }
                             }
                         }
@@ -138,7 +181,47 @@ if ($pagename == "constituency") {
                     }
                 });
 
-
+                function getImageName(vote) {
+                    switch (parseInt(vote)) {
+                        case 1:
+                            return 'support_icon';
+                        case -1:
+                            return 'dont_support_icon';
+                        case 0:
+                        default:
+                            return 'question_mark';
+                    }
+                };
+                
+                function getTooltip(vote) {
+                    switch (parseInt(vote)) {
+                        case 1:
+                            return 'Candidate answered yes to this question in the survey';
+                        case -1:
+                            return 'Candidate answered no to this question in the survey';
+                        case 0:
+                        default:
+                            return 'Candidate has not answered this survey question';
+                    }
+                };
+                
+                function getPartyLogoClass(party) {
+                    switch (party) {
+                        case 'Conservative':
+                            return 'conservative';
+                        case 'Liberal Democrat':
+                            return 'libdem';
+                        case 'Labour':
+                            return 'labour';
+                        case 'Green':
+                            return 'green';
+                        case 'UKIP':
+                            return 'ukip';
+                        default:
+                            return '';
+                    }
+                };
+                
 
         <?php
     } else {
